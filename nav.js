58247718d665ec872c15ce9820042f9c3e196979
YonkoSalar <salar@LAPTOP-QJIGC458.localdomain>
2020-12-16 11:19:19 +0100

    Navbar fix

--- nav.js
+++ nav.js
@@ -2,20 +2,22 @@ const sections = document.querySelectorAll('section');
 const line = document.querySelector('.line');
 
 const options = {
-    threshold: 0.9
+    threshold: 0.5
 };
 
 let observer = new IntersectionObserver(navCheck, options);
+
 function navCheck(entries){
     entries.forEach(entry => {
         const idName = entry.target.id;
-        const idName = entry.target.className;
-        const activeAnchor = document.querySelector(`[data-page=${className}]`);
+        const className = entry.target.className;
+        console.log(idName);
+        const activeAnchor = document.querySelector(`[data-page=${idName}]`);
         const coords = activeAnchor.getBoundingClientRect();
         const directions = {
-            height: coords.height,
+            height: coords.height - 25,
             width: coords.width,
-            top: coords.top,
+            top: coords.top + 35,
             left: coords.left,
 
         };
@@ -25,7 +27,7 @@ function navCheck(entries){
             line.style.setProperty('top', `${directions.top}px`);
             line.style.setProperty('width', `${directions.width}px`);
             line.style.setProperty('height', `${directions.height}px`);
-            line.style.background = blue;
+            line.style.background = "background: blue";
         }
     });
 
