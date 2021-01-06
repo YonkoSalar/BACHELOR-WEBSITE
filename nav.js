4cc37bc33b4e2eae3f56cf7c98c7db4b74a76d8b
Tobias <kjelltobias@gmail.com>
2021-01-06 22:31:00 +0100

    Live share

--- nav.js
+++ nav.js
@@ -14,12 +14,10 @@ let observer = new IntersectionObserver(navCheck, options);
 function navCheck(entries) {
     entries.forEach((entry) => {
         const idName = entry.target.id;
-        console.log(idName);
         const activeAnchor = document.querySelector(`[data-page=${idName}]`);
-        console.log(activeAnchor);
         const coords = activeAnchor.getBoundingClientRect();
         const directions = {
-            height: coords.height - 25,
+            height: coords.height - 35,
             width: coords.width,
             top: coords.top + 35,
             left: coords.left,
@@ -32,11 +30,7 @@ function navCheck(entries) {
             line.style.setProperty("height", `${directions.height}px`);
             line.style.background = "background: blue";
 
-            /* if (idName != "home") {
-                hide.classList.remove('hidden')
-            } else {
-                hide.classList.add('hidden')
-            } */
+        
         }
     });
 }
